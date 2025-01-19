b0fd559f17578e1e93c2319edfdf370f10caaadf
jor-eldevera <jorell.devera@gmail.com>
2025-01-19 00:34:14 -0800

    Write get and post routes

--- routes/MessageRouter.js
+++ routes/MessageRouter.js
@@ -1,5 +1,23 @@
 const { Router } = require("express");
+const { messages } = require("./IndexRouter");
 
 const messageRouter = Router();
+
+messageRouter.get("/", (req, res) => {
+    res.render("form");
+});
+
+messageRouter.post("/", (req, res) => {
+    let name = req.body.name;
+    let message = req.body.message;
+
+    messages.push({
+        text: message,
+        user: name,
+        added: new Date()
+    });
+
+    res.redirect("/");
+});
 
 module.exports = messageRouter;
