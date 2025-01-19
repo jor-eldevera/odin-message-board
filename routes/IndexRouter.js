b78575eff6b8dcbe2f27f841c0bedb4240994b9c
jor-eldevera <jorell.devera@gmail.com>
2025-01-18 23:02:40 -0800

    Add sample data

--- routes/IndexRouter.js
+++ routes/IndexRouter.js
@@ -2,4 +2,17 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Long live the Philippines",
+        user: "Jorell",
+        added: new Date()
+    },
+    {
+        text: "I hate United",
+        user: "Arne",
+        added: new Date()
+    }
+];
+
 module.exports = indexRouter;
