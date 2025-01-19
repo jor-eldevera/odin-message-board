ef65a578873655024f022601eb22e70905edf2ce
jor-eldevera <jorell.devera@gmail.com>
2025-01-18 23:15:22 -0800

    Use the index router

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ const PORT = 8080;
 const indexRouter = require("./routes/IndexRouter");
 const messageRouter = require("./routes/MessageRouter");
 
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
 app.listen(PORT);
