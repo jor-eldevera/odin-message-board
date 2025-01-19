0b8f41a957dc76576856e6eff61cfde2d58d8357
jor-eldevera <jorell.devera@gmail.com>
2025-01-19 00:33:06 -0800

    Add comments

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const app = express();
 
 const PORT = 8080;
 
-const indexRouter = require("./routes/IndexRouter");
+// Routers
+const { indexRouter, messages } = require("./routes/IndexRouter");
 const messageRouter = require("./routes/MessageRouter");
 
+// hooking up ejs
 const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// middleware to parse new message form data
+app.use(express.urlencoded({ extended: true }));
+
+// hooking up routers
 app.use("/", indexRouter);
+app.use("/new", messageRouter);
 
 app.listen(PORT);
