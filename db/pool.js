1bd1771feee22510e78ffc8360fad0ada111383d
jor-eldevera <jorell.devera@gmail.com>
2025-01-25 17:35:51 -0800

    Create Pool using env variables

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 module.exports = new Pool({
   host: process.env.PG_HOST, // or wherever the db is hosted
   user: process.env.PG_USER,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT // The default port
+  port: process.env.PG_PORT, // The default port
+  ssl: true
 });
